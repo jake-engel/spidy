5411c53fc08034cd9ca3db28fc649bea4335d7a0
Jake Engel <jakeengel01@gmail.com>
2017-11-21 11:04:39 +0100

    Updated routes, should be good enough for the basic start. More can be added later.

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,11 @@ Rails.application.routes.draw do
   resources :profiles, only: [ :show, :edit, :update ]
 
   resources :freelancers, only: [ :index ] do
-    resources :offers, only: [ :new, :create, :edit, :update ]
+    resources :offers, only: [ :new, :create, :update ]
   end
 
   namespace :profile do
-    resources :offers, only: [ :index, :edit, :update, :destroy ] do
+    resources :offers, only: [ :index, :update, :destroy ] do
       resources :messages, only: [ :index, :create, :update, :destroy ]
 
       member do
