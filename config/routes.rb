5eaa42e8fee0de0f2804d30625d4f125906dafe9
Jake Engel <jakeengel01@gmail.com>
2017-11-21 10:52:20 +0100

    Added routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users
+  authenticated do
+    root to: 'jobs#index'
+  end
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  resources :profiles, only: [ :show, :edit, :update ]
+
+  resources :freelancers, only: [ :index ] do
+    resources :offers, only: [ :new, :create, :edit, :update ]
+  end
+
+  namespace :profile do
+    resources :offers, only: [ :index, :edit, :update, :destroy ] do
+      resources :messages, only: [ :index, :create, :update, :destroy ]
+
+      member do
+        patch 'accept'
+        patch 'decline'
+        patch 'pending'
+      end
+    end
+  end
 end
