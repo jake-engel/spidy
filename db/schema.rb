27b26c0161dc970a3870ab688ed23843f1b79089
Daniel Warehime <daniel37@umbc.edu>
2017-11-21 12:48:49 +0100

    created a seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121091517) do
+ActiveRecord::Schema.define(version: 20171121113434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,8 @@ ActiveRecord::Schema.define(version: 20171121091517) do
   create_table "experiences", force: :cascade do |t|
     t.string "title"
     t.string "company"
-    t.string "date"
+    t.string "starting_date"
+    t.string "ending_date"
     t.string "location"
     t.string "picture", default: "SpidyLogo.svg"
     t.text "description"
