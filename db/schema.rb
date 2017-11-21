da3d2f9e3e28dd2ea3fa654e897ab41458f9622e
Daniel Warehime <daniel37@umbc.edu>
2017-11-21 10:25:12 +0100

    Added the continue with facebook account feature.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120154922) do
+ActiveRecord::Schema.define(version: 20171121091517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,11 @@ ActiveRecord::Schema.define(version: 20171120154922) do
     t.string "location"
     t.boolean "admin", default: false
     t.boolean "freelancer", default: false
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
